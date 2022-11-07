e5b352c4eb6fe2296fedc11d2b2b430b3ddf96a3
Julien Bernard <julienbernardjb88@gmail.com>
2022-11-07 13:59:51 +0100

    ajout Episode

---
+++ src/classes/dispatch/DispatcherEpisode.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace iutnc\NetVOD\dispatch;
+use iutnc\deefy\action;
+
+
+class DispatcherEpisode
+{
+    protected ?string $action = null;
+
+    public function __construct()
+    {
+        $this->action = isset($_GET['action']) ? $_GET['action'] : null;
+    }
+
+
+    public function run(): void
+    {
+        $html = '';
+        switch ($this->action) {
+            case 'ajout-commentaire':
+                $act = new action\AjoutCommentaireAction();
+                $html .= $act->execute();
+                break;
+            case 'ajout-note':
+                $act = new action\AjoutNoteAction();
+                $html .= $act->execute();
+                break;
+            case 'deconnexion':
+                $act = new action\DeconnexionAction();
+                $html .= $act->execute();
+            break;
+            case 'accueil-utilisateur':
+                $act = new action\AccueilUtilisateurAction();
+                $html .= $act->execute();
+                break;
+            default:
+                break;
+        }
+
+        $this->renderPage($html);
+    }
+
+
+    private function renderPage($html)
+    {
+        echo <<<END
+            <html lang="fr">
+                <head>
+                    <meta charset="UTF-8">>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>NetVOD</title>
+                </head>
+                <body>
+                    
+                </body>
+            </html>
+        END;
+    }
+
+}

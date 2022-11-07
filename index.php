94073818570e964963066666163a5b8d3075a075
Flotss <manginf54@gmail.com>
2022-11-07 10:25:24 +0100

    Correction index

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 
 declare(strict_types=1);
 
-use iutnc\deefy\dispatch\Dispatcher;
+use iutnc\NetVOD\dispatch\Dispatcher;
 
 session_start();
 
 require 'vendor/autoload.php';
-\iutnc\deefy\db\ConnectionFactory::setConfig('config.ini');
+\iutnc\NetVOD\db\ConnectionFactory::setConfig('config.ini');
 
 // DISPATCHER
 $d = new Dispatcher();

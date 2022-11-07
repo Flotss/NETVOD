80fb44b7a2bd007ebe5649823deb41fbe242882d
Romain.A <romain.auburtin1@etu.univ-lorraine.fr>
2022-11-07 17:40:25 +0100

    correction AffichageEpisodeAction.php

--- src/classes/action/AffichageEpisodeAction.php
+++ src/classes/action/AffichageEpisodeAction.php
@@ -2,20 +2,16 @@
 
 namespace iutnc\NetVOD\action;
 
+use iutnc\NetVOD\dispatch\DispatcherEpisode;
+
 class AffichageEpisodeAction extends Action
 {
 
     public function execute(): string
     {
-        $html = '';
-        try{
-            $db = ConnectionFactory::makeConnection();
-        }catch(DBExeption $e){
-            throw new AuthException($e->getMessage());
-        }
-        $q1 = $db->query("SELECT * from episode where id = ");//Ajouter un cookies pour savoir qu'elle episode a etais selectionner
-        $d1=$q1->fetch();
-        $html .= '<h4>' . $d1['titre'] . " Ep:" . $d1['numero'] . "</h4>" . "<video>" . $d1['file'] . "</video>" . "<p> durée:" . $d1['duree'] . "</p><p>Resume" . $d1['resume'] . "</p>";
+        header(Episode.php);
+        exit;
+        $html = ' ';
         return $html;
     }
 }

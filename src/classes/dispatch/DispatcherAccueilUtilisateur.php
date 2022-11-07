f1db718e4cd79402b7c2b628cd639b7bc446265b
Julien Bernard <julienbernardjb88@gmail.com>
2022-11-07 13:43:19 +0100

    ajout AccueilUtilisateur

---
+++ src/classes/dispatch/DispatcherAccueilUtilisateur.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace iutnc\NetVOD\dispatch;
+use iutnc\deefy\action;
+
+
+class DispatcherAccueilUtilisateur
+{
+    protected ?string $action = null;
+
+    public function __construct()
+    {
+        $this->action = isset($_GET['action']) ? $_GET['action'] : null;
+    }
+
+
+    public function run(): void
+    {
+        $html = '';
+        switch ($this->action) {
+            case 'affichage-serie':
+                $act = new action\AffichageSerieAction();
+                $html+= $act->execute();
+                break; //tous les cas d'inscription sont géré dans InscritpionAction
+            case 'deconnection':
+                $act = new action\DeonnectionAction();
+                $html+= $act->execute();
+            break;
+            default:
+                break;
+        }
+
+        $this->renderPage($html);
+    }
+
+
+    private function renderPage($html)
+    {
+        echo <<<END
+            <html lang="fr">
+                <head>
+                    <meta charset="UTF-8">>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>NetVOD</title>
+                </head>
+                <body>
+                    
+                </body>
+            </html>
+        END;
+    }
+
+}

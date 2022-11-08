a41c8fe841248e9c895ecf40aa46053302b3b1d3
Romain.A <romain.auburtin1@etu.univ-lorraine.fr>
2022-11-08 15:54:23 +0100

    Mise a jour des requetes de AjoutCommentaireAction.php and de AjoutNoteAction.php

--- src/classes/action/AjoutNoteAction.php
+++ src/classes/action/AjoutNoteAction.php
@@ -10,18 +10,19 @@ class AjoutNoteAction extends Action
     public function execute(): string
     {
         $html = '';
+        $titre = "Le lac";
         try {
             $db = ConnectionFactory::makeConnection();
         } catch (DBExeption $e) {
             throw new AuthException($e->getMessage());
         }
-        $q1 = $db->query("SELECT note from serieComNote where id_user = " . $_SESSION['id'] . " AND id_serie = 1");
+        $q1 = $db->query("SELECT note from serieComNote, episode where serieComNote.id_serie = episode.serie_id AND id_user = " . $_SESSION['id'] . " AND titre = '" . $titre . "'");
         if(!$d1=$q1->fetch()){
             if ($this->http_method === 'GET') {
                 $html .= <<<END
                 <form method="post" action="?action=signin">
                     <label>Note :<input type="number" name="note" placeholder="<note>"></label>
-                    <button type="submit">Connexion</button>
+                    <button type="submit">Noter</button>
                 </form>
             END;
             } else { // POST
@@ -31,11 +32,11 @@ class AjoutNoteAction extends Action
                 } catch (DBExeption $e) {
                     throw new AuthException($e->getMessage());
                 }
-                $q2 = $db->query("SELECT * from serieComNote where id_user = " . $_SESSION['id'] . " AND id_serie = 1");
+                $q2 = $db->query("SELECT * from serieComNote, episode where serieComNote.serie_id = episode.serie_id AND id_user = " . $_SESSION['id'] . " titre = '" . $titre . "'");
                 if($q2['id_user'] == null){
                     $insert = $db->exec("INSERT INTO serieComNote(id_user,id_serie,note) VALUES(" . $_SESSION['id'] . ",1," . $note );
                 }else{
-                    $insert = $db->exec("INSERT INTO serieComNote(note) where id_user =" . $_SESSION['id'] . " AND id_serie = 1" . " VALUE(" . $note . ")");
+                    $insert = $db->exec("INSERT INTO serieComNote(note) where id_user =" . $_SESSION['id'] . " AND id_serie = (SELECT serie_id from episode where titre = '" . $titre . "')" . " VALUE(" . $note . ")");
                 }
                 $html = "commentaire ajoutée";
             }

ad020c2c335d8861d7d35bb203ddb8b949a88654
Julien Bernard <julienbernardjb88@gmail.com>
2022-11-08 16:50:52 +0100

    modif AffichageDetailleeSerieAction.php modif affichage

--- src/classes/action/AffichageDetailleeSerieAction.php
+++ src/classes/action/AffichageDetailleeSerieAction.php
@@ -17,23 +17,36 @@ class AffichageDetailleeSerieAction extends Action
             throw new AuthException($e->getMessage());
         }
         $_COOKIE['nomSerie'] = str_replace("'","\'",$_COOKIE['nomSerie']);
-        $infoSerie = $db->query("SELECT * from serie where titre = '{$_COOKIE['nomSerie']}'");
+        $infoSerie = $db->query("SELECT s.titre as titre, s.descriptif, date_ajout, annee, img, s.id as id, COUNT(e.numero) as nbEp from serie s INNER JOIN episode e ON s.id = e.serie_id where s.titre = '{$_COOKIE['nomSerie']}' GROUP BY (s.titre)  ");
         $infoSerie = $infoSerie->fetch();
         $html .= <<<END
                 <h2>  {$infoSerie['titre']}  </h2>
                 <img alt="" src="{$infoSerie['img']}">
                 <p>{$infoSerie['descriptif']}</p>
+                <p>genre: ??</p>
+                <p>public visée: ??</p>
                 <p>Date d'ajout : {$infoSerie['date_ajout']}</p>
-                <p>Année : {$infoSerie['annee']}</p>
+                <p>Année de sortie : {$infoSerie['annee']}</p>
+                <p>Nombre d'épisode : {$infoSerie['nbEp']}</p>
                 END;
 
-        $infoSaison = $db->query("SELECT numero from episode where serie_id = {$infoSerie['id']}");
-        while ($infoSaison = $infoSaison->fetch()){
+        $infoSaison = $db->query("SELECT * from episode where serie_id = {$infoSerie['id']}");
+        while ($infoSaisons = $infoSaison->fetch()){
             $html .= <<<END
-                <h2>Saison {$infoSaison['numero']}</h2>
+                <h2>Episode {$infoSaisons['numero']}</h2>
+                <p>Titre {$infoSaisons['titre']}</p>
+                <p>Durée:  {$infoSaisons['duree']}</p>
                 END;
         }
 
         return $html;
     }
 }
+
+/*
+ *
+. La liste des
+épisodes de la série est également affichée, avec pour chaque épisode : numéro, titre, durée,
+image.
+
+ */

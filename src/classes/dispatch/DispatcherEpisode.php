38e4fdcc8c15668b5a8506a752139e6439ee7b74
Flotss <manginf54@gmail.com>
2022-11-09 14:53:53 +0100

    Ajout de la video, correction sql

--- src/classes/dispatch/DispatcherEpisode.php
+++ src/classes/dispatch/DispatcherEpisode.php
@@ -74,13 +74,32 @@ class DispatcherEpisode
         }else {
             Redirection::redirection('PageSerie');
         }
+
         $q1 = $db->query("SELECT file,numero,duree,resume,episode.titre,serie.titre AS serieTitre from episode,serie where episode.serie_id = serie.id AND episode.titre = '" . $titre . "'");
         $d1=$q1->fetch();
-        $episode = '<h4>' . $d1['serieTitre'] . ". Episode " . $d1['numero'] . ": " . $d1['titre'] . "</h4>" . "<video>" . $d1['file'] . "</video>" . "<p> Durée: " . $d1['duree'] . " minute</p><p>Resume: " . $d1['resume'] . "</p>";
+
+        // Chemin pour trouver le fichier
+        $scriptNameExplode = explode('/', $_SERVER['SCRIPT_NAME']);
+        $chemin = '';
+        for ($k = 0; $k < count($scriptNameExplode) - 1; $k++) {
+            $chemin .= $scriptNameExplode[$k] . '/';
+        }
+        $episode = <<<END
+            <h4> {$d1['serieTitre']} - Episode {$d1['numero']} </h4>
+            <video controls width="1080">
+                    <source src="{$chemin}/ressource/video/{$d1['file']}" type="video/mp4">
+            </video>
+            <p> Durée : {$d1['duree']} secondes </p>  
+            <p> Résumé : {$d1['resume']} </p>
+        END;
+
         $q2 = $db->query("SELECT etat from etatserie where id_user = " . $_SESSION['id'] . " AND id_serie = (SELECT serie_id from episode where titre = '" . $titre . "')");
         if(!$d2=$q2->fetch()){
-            $insert = $db->exec("INSERT INTO etatserie(id_user,id_serie,etat) VALUES(" . $_SESSION['id'] . ",(SELECT serie_id from episode where titre = '" . $titre . "'),'en cours')");
+            $db->exec("INSERT INTO etatserie(id_user,id_serie,etat) VALUES(" . $_SESSION['id'] . ",(SELECT serie_id from episode where titre = '" . $titre . "'),'en cours')");
         }
+
+
+
         $comment = "<p>Vous aimez l'épisode " . $_SESSION['user'] . " ? n'ésitait pas a commenter et laisser une note!</p>";
         echo <<<END
             <html lang="fr">

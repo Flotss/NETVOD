3264ed7c71495b41f498bdb860a55c47e40cb6d4
Romain.A <romain.auburtin1@etu.univ-lorraine.fr>
2022-11-09 10:06:07 +0100

    Correction d'une erreur dans une requete SQL

--- src/classes/action/AjoutCommentaireAction.php
+++ src/classes/action/AjoutCommentaireAction.php
@@ -36,7 +36,7 @@ class AjoutCommentaireAction extends Action
                 if(!$d2 = $q2->fetch()){
                     $insert = $db->exec("INSERT INTO serieComNote(id_user,id_serie,commentaire) VALUES(" . $_SESSION['id'] . ", (SELECT serie_id from episode where titre = '" . $titre . "') ,'" . $com . "')");
                 }else{
-                    $insert = $db->exec("UPDATE serieComNote SET commentaire = " . $com . " where id_user =" . $_SESSION['id'] . " AND id_serie = (SELECT serie_id from episode where titre = '" . $titre . "')");
+                    $insert = $db->exec("UPDATE serieComNote SET commentaire = '" . $com . "' where id_user =" . $_SESSION['id'] . " AND id_serie = (SELECT serie_id from episode where titre = '" . $titre . "')");
                 }
                 $html = "commentaire ajoutée";
             }

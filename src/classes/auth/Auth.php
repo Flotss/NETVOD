0d926ba836bcc5c8a339de23efc40a06df8e5cc2
Julien Bernard <julienbernardjb88@gmail.com>
2022-11-10 11:29:55 +0100

    correctif nom dans bd (doublons)

--- src/classes/auth/Auth.php
+++ src/classes/auth/Auth.php
@@ -64,7 +64,7 @@ class Auth
         if ($stmt->fetch()) throw new AuthException("compte deja existant");
 
         try {
-            $query = "insert into user (email, password, nom, prenom, genre, public) values (?, ?, ?, ?, '', '')";
+            $query = "insert into user (email, password, nom, prenom, genreUser, publicUser) values (?, ?, ?, ?, '', '')";
             $stmt = $db->prepare($query);
             $stmt->execute([$email, $hash, $nom, $prenom]);
 

203e813a713ad29ae067419ba8ab44f7d835863e
Julien Bernard <julienbernardjb88@gmail.com>
2022-11-10 10:10:08 +0100

    ajout public et genre de user

--- src/classes/action/GestionCompteAction.php
+++ src/classes/action/GestionCompteAction.php
@@ -16,17 +16,27 @@ class GestionCompteAction extends Action
         $infoUser = $db->query("SELECT * from user where id = " . $_SESSION['id']);
         $infoUser = $infoUser->fetch();
         $listGenre = $db->query("SELECT * from genre");
-        $litbutton ='';
+        $listPublic = $db->query("SELECT * from public");
+        $listbuttonGenre ='';
         while ($g = $listGenre->fetch()){
-            $litbutton .= "<input type='submit' value='{$g['libele']}' name='{$g['libele']}'>";
+            $listbuttonGenre .= <<<END
+ <option value="{$g['libele']}" >{$g['libele']}</option> 
+END;
+        }
+
+        $listbuttonPublic ='';
+        while ($g = $listPublic->fetch()){
+            $listbuttonPublic .= <<<END
+ <option value="{$g['libele']}" name="{$g['libele']}" >{$g['libele']}</option> 
+END;
+
         }
 
         if ($this->http_method == 'GET'){
-            return $this->getForm($infoUser, $litbutton);
+            return $this->getForm($infoUser, $listbuttonGenre, $listbuttonPublic);
         }else { // POST
 
             if (isset($_POST['valider'])) {
-
 
                 // Filtre les entrées
                 $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
@@ -40,48 +50,68 @@ class GestionCompteAction extends Action
                     $db->query("UPDATE user SET nom = '$nom', prenom = '$prenom', email = '$email' WHERE id = " . $_SESSION['id']);
                     $infoUser = $db->query("SELECT * from user where id = " . $_SESSION['id']);
                     $infoUser = $infoUser->fetch();
-                    $html = $this->getForm($infoUser,$litbutton);
+                    $html = $this->getForm($infoUser,$listbuttonGenre, $listbuttonPublic);
                     $html .= '<p>Vos informations ont bien été modifiées</p>';
                     $_SESSION['user'] = $infoUser['prenom'];
                     return $html;
                 } else {
-                    $html .= $this->getForm($infoUser,$litbutton);
+                    $html .= $this->getForm($infoUser,$listbuttonGenre, $listbuttonPublic);
                     $html .= 'Mot de passe incorrect';
                     return $html;
                 }
-            } else {
-                $listGenre = $db->query("SELECT * from genre");
-                while ($g = $listGenre->fetch()){
-                    if (isset($_POST["{$g['libele']}"])) {
-                        $genre = "{$g['libele']}";
-                        if(strpos("{$infoUser['genre']}",$genre)===false){
-                            $db->query("UPDATE user SET genre = '{$infoUser['genre']} $genre' WHERE id = " . $_SESSION['id']);
-                        }else{
-                            $nGenre = explode($genre,"{$infoUser['genre']}");
-                            $val="{$nGenre[0]} {$nGenre[1]}";
-                            if("{$nGenre[0]}"===" "||"{$nGenre[0]}"==="  "){
-                                print "supr debut";
-                                $val = "{$nGenre[1]}";
-                            }else if("{$nGenre[1]}"===" "||"{$nGenre[1]}"==="  "){
-                                print "supr fin";
-                                $val = "{$nGenre[0]}";
-                            }
-                            $val = str_replace("  "," ",$val);
-                            $db->query("UPDATE user SET genre = '$val' WHERE id = " . $_SESSION['id']);
+            } else if (isset($_POST['genre'])){
+                $genre = $_POST["selectgenre"];
+                if($genre != '') {
+                    if (strpos("{$infoUser['genre']}", $genre) === false) {
+                        $db->query("UPDATE user SET genre = '{$infoUser['genre']} $genre' WHERE id = " . $_SESSION['id']);
+                    } else {
+                        $nGenre = explode($genre, "{$infoUser['genre']}");
+                        $val = "{$nGenre[0]} {$nGenre[1]}";
+                        if ("{$nGenre[0]}" === " " || "{$nGenre[0]}" === "  ") {
+                            $val = "{$nGenre[1]}";
+                        } else if ("{$nGenre[1]}" === " " || "{$nGenre[1]}" === "  ") {
+                            $val = "{$nGenre[0]}";
                         }
-                        $infoUser = $db->query("SELECT * from user where id = " . $_SESSION['id']);
-                        $infoUser = $infoUser->fetch();
-
+                        $val = str_replace("  ", " ", $val);
+                        $db->query("UPDATE user SET genre = '$val' WHERE id = " . $_SESSION['id']);
                     }
+                    $infoUser = $db->query("SELECT * from user where id = " . $_SESSION['id']);
+                    $infoUser = $infoUser->fetch();
                 }
 
-                $html .= $this->getForm($infoUser,$litbutton);
+
+
+                $html .= $this->getForm($infoUser,$listbuttonGenre, $listbuttonPublic);
+                return $html;
+            }else{
+
+                $public = $_POST["selectpublic"];
+                $public = str_replace("'", "\'", $public);
+                if($public != '') {
+                    if (strpos("{$infoUser['public']}", $public) === false) {
+                        $db->query("UPDATE user SET public = '{$infoUser['public']} $public' WHERE id = " . $_SESSION['id']);
+                    } else {
+                        $nPublic = explode($public, "{$infoUser['public']}");
+                        $val = "{$nPublic[0]} {$nPublic[1]}";
+                        if ("{$nPublic[0]}" === " " || "{$nPublic[0]}" === "  ") {
+                            $val = "{$nPublic[1]}";
+                        } else if ("{$nPublic[1]}" === " " || "{$nPublic[1]}" === "  ") {
+                            $val = "{$nPublic[0]}";
+                        }
+                        $val = str_replace("  ", " ", $val);
+                        $db->query("UPDATE user SET public = '$val' WHERE id = " . $_SESSION['id']);
+                    }
+                    $infoUser = $db->query("SELECT * from user where id = " . $_SESSION['id']);
+                    $infoUser = $infoUser->fetch();
+                }
+
+                $html .= $this->getForm($infoUser,$listbuttonGenre, $listbuttonPublic);
                 return $html;
             }
         }
     }
 
-    private function getForm($infoUser, $litbutton): string{
+    private function getForm($infoUser, $listbuttonGenre, $listbuttonPublic ): string{
         return  <<<END
             <h3>Vous pouvez ici changer vos informations</h3>
             <div class="enteteAccueil">
@@ -96,7 +126,20 @@ class GestionCompteAction extends Action
                 <input type="submit" value="Valider" name="valider">
                 <label for="genre">Vos genres préféré: {$infoUser['genre']}</label>
                 <label for="genre">Ajouter/ supprimer un genre:</label>
-                $litbutton
+                
+                    <select name="selectgenre" >
+                        <option value='' ></option>
+                        $listbuttonGenre
+                    </select>
+                    <button type='submit' name="genre">Ajouter / Supprimer</button>
+                
+                <label for="genre">Vos public: {$infoUser['public']}</label>
+                <label for="genre">Ajouter/ supprimer un public:</label>
+                    <select name=selectpublic >
+                        <option value='' ></option>
+                $listbuttonPublic
+                </select>
+                    <button type='submit' name="public">Ajouter / Supprimer</button>
                 
             </form>
             
